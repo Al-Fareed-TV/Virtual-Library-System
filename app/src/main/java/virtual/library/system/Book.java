68917b110abbb7b4a388dc4796ff9dc78db4ea4a
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-11-30 07:44:53 +0530

    added a mechanism to batch upload multiple books

--- app/src/main/java/virtual/library/system/Book.java
+++ app/src/main/java/virtual/library/system/Book.java
@@ -1,6 +1,9 @@
 package virtual.library.system;
 
 import java.time.LocalDate;
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+
 public class Book {
     private String author;
     private String title;
@@ -8,7 +11,7 @@ public class Book {
     private String genre;
     private LocalDate publicationDate;
     private int numberOfCopies;
-   
+
     public Book() {
     }
 
@@ -19,5 +22,26 @@ public class Book {
         this.genre = genre;
         this.publicationDate = publicationDate;
         this.numberOfCopies = numberOfCopies;
+    }
+
+    public void batchUploadFromCSV(String csvFilePath)throws Exception {
+        try (CSVReader reader = new CSVReader(new FileReader(csvFilePath))) {
+            String[] nextRecord;
+            while ((nextRecord = reader.readNext()) != null) {
+                // Assuming the CSV format is: author, title, isbn, genre, publicationDate, numberOfCopies
+                String author = nextRecord[0];
+                String title = nextRecord[1];
+                String isbn = nextRecord[2];
+                String genre = nextRecord[3];
+                LocalDate publicationDate = LocalDate.parse(nextRecord[4]);
+                int numberOfCopies = Integer.parseInt(nextRecord[5]);
+
+                // Create and save the book
+                Book newBook = new Book(author, title, isbn, genre, publicationDate, numberOfCopies);
+                // Save the newBook to your library
+            }
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
     }
 }
