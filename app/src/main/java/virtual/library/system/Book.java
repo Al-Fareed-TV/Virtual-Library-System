468fc98a0eaf80805a48fdfc57ec6d98f4efa5ff
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-11-30 08:02:13 +0530

    moved method batchUpload from Book to Library Class, and modified to check for uniqueness of the isbn and print how many books are added and skipped

--- app/src/main/java/virtual/library/system/Book.java
+++ app/src/main/java/virtual/library/system/Book.java
@@ -24,24 +24,5 @@ public class Book {
         this.numberOfCopies = numberOfCopies;
     }
 
-    public void batchUploadFromCSV(String csvFilePath)throws Exception {
-        try (CSVReader reader = new CSVReader(new FileReader(csvFilePath))) {
-            String[] nextRecord;
-            while ((nextRecord = reader.readNext()) != null) {
-                // Assuming the CSV format is: author, title, isbn, genre, publicationDate, numberOfCopies
-                String author = nextRecord[0];
-                String title = nextRecord[1];
-                String isbn = nextRecord[2];
-                String genre = nextRecord[3];
-                LocalDate publicationDate = LocalDate.parse(nextRecord[4]);
-                int numberOfCopies = Integer.parseInt(nextRecord[5]);
-
-                // Create and save the book
-                Book newBook = new Book(author, title, isbn, genre, publicationDate, numberOfCopies);
-                // Save the newBook to your library
-            }
-        } catch (Exception e) {
-            System.out.println(e.getLocalizedMessage());
-        }
-    }
+    
 }
--- app/src/main/java/virtual/library/system/Library.java
+++ app/src/main/java/virtual/library/system/Library.java
@@ -1,14 +1,49 @@
 package virtual.library.system;
 
+import java.io.FileReader;
+import java.time.LocalDate;
 import java.util.*;
+
+import com.opencsv.CSVReader;
 
 public class Library {
     private Set<String> isbnSet;
+
     public Library() {
         this.isbnSet = new HashSet<>();
     }
 
-    public boolean isIsbnUnique(String isbn) {
+    private boolean isIsbnUnique(String isbn) {
         return isbnSet.add(isbn);
+    }
+
+    public void batchUploadFromCSV(String csvFilePath) throws Exception {
+        int booksAdded = 0;
+        int booksSkipped = 0;
+        try (CSVReader reader = new CSVReader(new FileReader(csvFilePath))) {
+            String[] nextRecord;
+            while ((nextRecord = reader.readNext()) != null) {
+                if (isIsbnUnique(nextRecord[2])) {
+                    String author = nextRecord[0];
+                    String title = nextRecord[1];
+                    String isbn = nextRecord[2];
+                    String genre = nextRecord[3];
+                    LocalDate publicationDate = LocalDate.parse(nextRecord[4]);
+                    int numberOfCopies = Integer.parseInt(nextRecord[5]);
+
+                    Book newBook = new Book(author, title, isbn, genre, publicationDate, numberOfCopies);
+                    booksAdded++;
+                }else{
+                    booksSkipped++;
+                }
+            }
+            booksUploadedAndSkipped(booksAdded, booksSkipped);
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+    private void booksUploadedAndSkipped(int booksAdded, int booksSkipped){
+        System.out.println("Number of Books Added : " + booksAdded+"\nNumber of books skipped : " + booksSkipped);
+
     }
 }
