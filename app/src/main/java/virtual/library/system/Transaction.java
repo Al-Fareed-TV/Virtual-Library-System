11fc421aa5bad907ba1ceefc80ae72dc95eb932d
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-12-01 20:32:48 +0530

    final channge for Show Book Details in CLI

--- app/src/main/java/virtual/library/system/Transaction.java
+++ app/src/main/java/virtual/library/system/Transaction.java
@@ -1,6 +1,5 @@
 package virtual.library.system;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,19 +8,19 @@ import com.opencsv.exceptions.CsvValidationException;
 public class Transaction {
     public static void main(String[] args) throws CsvValidationException {
         Scanner input = new Scanner(System.in);
-        Library library = new Library(); 
+        Library library = new Library();
         List<Book> bookList = library.getListOfBooks();
 
         library.displayAllBooks();
 
-       System.out.print("Seelect a book from list : ");
-       int index = input.nextInt();
-       
-       library.displaySelectedBook(bookList.get(index-1));
-       
+        System.out.print("Please enter the number next to a book to view its details: ");
+        int selectedBookIndex = input.nextInt() - 1;
+
+        if (selectedBookIndex >= 0 && selectedBookIndex < bookList.size()) {
+            library.displaySelectedBook(bookList.get(selectedBookIndex));
+        } else {
+            System.out.println("Invalid selection. Please choose a number from the list.");
+        }
+
     }
 }
-
-
-
-
