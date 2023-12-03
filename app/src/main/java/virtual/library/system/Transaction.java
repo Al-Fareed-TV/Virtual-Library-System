351b53e892b2e89e06dbd00ea491b5175a7df8f9
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-12-03 21:15:04 +0530

    new changes

--- app/src/main/java/virtual/library/system/Transaction.java
+++ app/src/main/java/virtual/library/system/Transaction.java
@@ -6,14 +6,18 @@ import java.util.Scanner;
 import com.opencsv.exceptions.CsvValidationException;
 
 public class Transaction {
-    public static void main(String[] args){
+    private static boolean isValidISBN(String isbn) {
+        return isbn.matches("[0-9-]+") && isbn.replaceAll("-", "").length() == 13;
+    }
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Library library = null;
         try {
             library = new Library();
         } catch (CsvValidationException e) {
-            System.out.println("Failed to load library due to CSV formatting error.\nExiting..!");
-            System.exit(0);
+            input.close();
+            throw new IllegalStateException("Failed to load library due to CSV formatting error.");
         }
         List<Book> bookList = library.getListOfBooks();
 
@@ -23,7 +27,7 @@ public class Transaction {
         List<Book> searchedBooks = library.searchBooks(criteria);
         library.displayAllBooks(searchedBooks);
         System.out.print("Enter the book number for more details: ");
-        int bookIndex = input.nextInt() - 1; 
+        int bookIndex = input.nextInt() - 1;
         if (bookIndex >= 0 && bookIndex < searchedBooks.size()) {
             Book selectedBook = searchedBooks.get(bookIndex);
             library.displaySelectedBook(selectedBook);
@@ -32,28 +36,28 @@ public class Transaction {
         }
 
         System.out.println("Please enter the ISBN of the book : ");
-String isbn = input.nextLine();
-if (!isValidISBN(isbn)) {
-    System.out.println("Invalid ISBN format. Please enter a valid ISBN.");
-    return;
-}
-List<Book> bookByISBN = library.searchBooks(isbn);
-library.displayAllBooks(bookByISBN);
-System.out.println("Confirm the Title of the book (y/n): ");
-String confirmTitle = input.next();
-if (confirmTitle.equalsIgnoreCase("y")) {
-    System.out.println("Proceed(y)/Cancel(n)? : ");
-    char proceed = input.next().charAt(0);
-    if (proceed == 'y') {
-        System.out.println("Book issued...");
-    } else {
-        System.out.println("Canceled transaction");
-        input.close();
-        return;
-    }
-} else if (!confirmTitle.equalsIgnoreCase("n")) {
-    System.out.println("Invalid option..");
-}
+        String isbn = input.nextLine();
+        if (!isValidISBN(isbn)) {
+            System.out.println("Invalid ISBN format. Please enter a valid ISBN.");
+            return;
+        }
+        List<Book> bookByISBN = library.searchBooks(isbn);
+        library.displayAllBooks(bookByISBN);
+        System.out.println("Confirm the Title of the book (y/n): ");
+        String confirmTitle = input.next();
+        if (confirmTitle.equalsIgnoreCase("y")) {
+            System.out.println("Proceed(y/n)? : ");
+            char proceed = input.next().charAt(0);
+            if (proceed == 'y') {
+                System.out.println("Book issued...");
+            } else {
+                System.out.println("Canceled transaction");
+                input.close();
+                return;
+            }
+        } else if (!confirmTitle.equalsIgnoreCase("n")) {
+            System.out.println("Invalid option..");
+        }
         input.close();
     }
 }
