8b5e5e5e3b133e18e8c5c8a5e0c1bd5aa133c829
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-12-04 16:39:58 +0530

    stores transaction data undefined

--- app/src/main/java/virtual/library/system/Transaction.java
+++ app/src/main/java/virtual/library/system/Transaction.java
@@ -1,33 +1,33 @@
 package virtual.library.system;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 import com.opencsv.exceptions.CsvValidationException;
 
 public class Transaction {
+    private static final Scanner input = new Scanner(System.in);
+    private static final Map<String, String> transactionLog = new HashMap<>();
+
     private static boolean isValidISBN(String isbn) {
         return isbn.matches("[0-9-]+") && isbn.replaceAll("-", "").length() == 13;
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Library library = null;
-        try {
-            library = new Library();
-        } catch (CsvValidationException e) {
-            input.close();
-            throw new IllegalStateException("Failed to load library due to CSV formatting error.");
-        }
-        List<Book> bookList = library.getListOfBooks();
+        Library library = initializeLibrary();
 
+        List<Book> bookList = library.getListOfBooks();
         library.displayAllBooks(bookList);
-        System.out.println("Enter Book's Title/Author/ISBN :");
-        String criteria = input.nextLine();
+
+        String criteria = getUserInput("Enter Book's Title/Author/ISBN:");
         List<Book> searchedBooks = library.searchBooks(criteria);
         library.displayAllBooks(searchedBooks);
-        System.out.print("Enter the book number for more details: ");
-        int bookIndex = input.nextInt() - 1;
+
+        int bookIndex = getBookIndexFromUser(searchedBooks.size());
         if (bookIndex >= 0 && bookIndex < searchedBooks.size()) {
             Book selectedBook = searchedBooks.get(bookIndex);
             library.displaySelectedBook(selectedBook);
@@ -35,29 +35,70 @@ public class Transaction {
             System.out.println("Invalid selection. Please enter a valid book number.");
         }
 
-        System.out.println("Please enter the ISBN of the book : ");
-        String isbn = input.nextLine();
+        String isbn = getUserInput("Please enter the ISBN of the book:");
         if (!isValidISBN(isbn)) {
             System.out.println("Invalid ISBN format. Please enter a valid ISBN.");
             return;
         }
+
         List<Book> bookByISBN = library.searchBooks(isbn);
         library.displayAllBooks(bookByISBN);
-        System.out.println("Confirm the Title of the book (y/n): ");
-        String confirmTitle = input.next();
-        if (confirmTitle.equalsIgnoreCase("y")) {
-            System.out.println("Proceed(y/n)? : ");
-            char proceed = input.next().charAt(0);
-            if (proceed == 'y') {
+
+        if (confirmAction("Confirm the Title of the book (y/n):")) {
+            String userId = getUserInput("Enter your User ID:");
+            if (confirmAction("Proceed (y/n)?")) {
+                recordTransaction(userId, isbn);
                 System.out.println("Book issued...");
             } else {
                 System.out.println("Canceled transaction");
                 input.close();
                 return;
             }
-        } else if (!confirmTitle.equalsIgnoreCase("n")) {
-            System.out.println("Invalid option..");
+        } else {
+            System.out.println("Invalid option.");
         }
+
         input.close();
+    }
+
+    private static Library initializeLibrary() {
+        try {
+            return new Library();
+        } catch (CsvValidationException e) {
+            input.close();
+            throw new IllegalStateException("Failed to load library due to CSV formatting error.");
+        }
+    }
+
+    private static String getUserInput(String prompt) {
+        System.out.println(prompt);
+        return input.nextLine();
+    }
+
+    private static int getBookIndexFromUser(int maxIndex) {
+        System.out.print("Enter the book number for more details:");
+        try {
+            return Integer.parseInt(input.nextLine()) - 1;
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    private static boolean confirmAction(String prompt) {
+        System.out.print(prompt);
+        String response = input.nextLine();
+        return response.equalsIgnoreCase("y");
+    }
+
+    private static void recordTransaction(String userId, String isbn) {
+        Date borrowingDate = new Date();
+        transactionLog.put(userId, "ISBN: " + isbn + ", Borrowing Date: " + borrowingDate);
+    }
+
+    public static void viewTransactionLog() {
+        System.out.println("Transaction Log:");
+        for (Map.Entry<String, String> entry : transactionLog.entrySet()) {
+            System.out.println("User ID: " + entry.getKey() + ", Transaction: " + entry.getValue());
+        }
     }
 }
--- app/src/main/java/virtual/library/system/User.java
+++ app/src/main/java/virtual/library/system/User.java
@@ -5,5 +5,8 @@ public class User {
     private int userId;
     private String userName;
     private String email;
+    public int getUserId() {
+        return userId;
+    }
     
 }
